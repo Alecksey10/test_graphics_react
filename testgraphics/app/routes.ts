68da2d0dda44579baf6b2353001cb69bfb778b26
alecksey10 <alecksey10@mail.ru>
2026-06-28 19:59:05 +0700

    test2

--- testgraphics/app/routes.ts
+++ testgraphics/app/routes.ts
@@ -1,5 +1,7 @@
 import { type RouteConfig, index, route} from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx"),
-    route("/part1/try1", "./routes/part1try1.tsx")
- ] satisfies RouteConfig;
+export default [
+  index("./routes/home.tsx"), // вместо "./routes/index.tsx"
+  route("test_graphics_react/", "./routes/index.tsx"),
+  route("test_graphics_react/part1/try1", "./routes/part1try1.tsx")
+] satisfies RouteConfig;
